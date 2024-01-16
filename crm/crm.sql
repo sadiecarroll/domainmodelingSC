11c856d9332b8b49089bac30fe19779b379eaca1
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-01-16 17:21:17 +0000

    created tables

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,22 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number INTEGER,
+    company_name TEXT
+);
+
+CREATE TABLE activites (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     type TEXT,
+     date INTEGER, 
+     time INTEGER, 
+     notes TEXT,
+     
+     
+)
